98768ab6813df5c04a3903341021fd6e6c650b80
Francisco Gomez De Travecedo Mendez <frangome@c1r3s1.42malaga.com>
2023-05-08 21:57:25 +0200

    finishing gnl

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,27 +6,28 @@
 /*   By: frangome <frangome@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 12:18:27 by fran              #+#    #+#             */
-/*   Updated: 2023/05/06 19:05:19 by frangome         ###   ########.fr       */
+/*   Updated: 2023/05/08 20:25:07 by frangome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GETNEXTLINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 75
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 75
+# endif
 
-# define GETNEXTLINE_H
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-
-char            *get_next_line(int fd);
-int             nline(char *buffer);
-void	        remove_line(char *buffer);
-char	        *ft_strjoin(char const *s1, char const *s2);
-void	        fill_line(char *line, char *buffer);
-int	            is_line(char *buffer);
+char			*get_next_line(int fd);
+int				nline(char *buffer);
+void			remove_line(char *buffer);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			fill_line(char *line, char *buffer);
+int				is_line(char *buffer);
 unsigned int	ft_strlen(const char *str);
+char			*no_line(char *line, char *buffer, int fd, int i);
+void			empty_buffer(char *buffer);
 
 #endif
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: frangome <frangome@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 12:27:32 by fran              #+#    #+#             */
-/*   Updated: 2023/05/06 19:01:45 by frangome         ###   ########.fr       */
+/*   Updated: 2023/05/08 20:23:19 by frangome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int	is_line(char *buffer)
 {
 	int	i;
-	
+
 	i = 0;
-	while (buffer[i] != 0)
+	while (i < BUFFER_SIZE && buffer[i] != 0)
 	{
 		if (buffer[i] == '\n')
 			return (1);
@@ -31,9 +31,9 @@ int	nline(char *buffer)
 	int	i;
 
 	i = 0;
-	while (buffer[i] != '\n' && buffer[i] != '\0' && i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE && buffer[i] != '\n' && buffer[i] != '\0')
 		i++;
-	if (buffer[i] == '\n')
+	if (i < BUFFER_SIZE && buffer[i] == '\n')
 		i++;
 	return (i);
 }
@@ -45,8 +45,7 @@ void	remove_line(char *buffer)
 
 	size = nline(buffer);
 	i = 0;
-	
-	while (buffer[i] != '\0' && i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE && buffer[i] != '\0')
 	{
 		if (i < (BUFFER_SIZE - size))
 			buffer[i] = buffer[i + size];
@@ -55,22 +54,49 @@ void	remove_line(char *buffer)
 		i++;
 	}
 }
-	
 
 void	fill_line(char *line, char *buffer)
 {
 	int	i;
 
 	i = 0;
-	while(buffer[i] != '\n' && buffer[i] != '\0' && i < BUFFER_SIZE)
+	while (i < BUFFER_SIZE && buffer[i] != '\n' && buffer[i] != '\0')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
-	if (buffer[i] == '\n')
+	if (i < BUFFER_SIZE && buffer[i] == '\n')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
 	line[i] = 0;
+}
+
+char	*no_line(char *line, char *buffer, int fd, int i)
+{
+	remove_line(buffer);
+	i = read(fd, buffer, BUFFER_SIZE);
+	if (i < 0)
+	{
+		free(line);
+		empty_buffer(buffer);
+		return (0);
+	}
+	while (i && !is_line(buffer))
+	{
+		line = ft_strjoin(line, buffer);
+		remove_line(buffer);
+		i = read(fd, buffer, BUFFER_SIZE);
+		if (i < 0)
+		{
+			free(line);
+			empty_buffer(buffer);
+			return (0);
+		}
+	}
+	if (i)
+		line = ft_strjoin(line, buffer);
+	remove_line(buffer);
+	return (line);
 }
